e5169ab78dfd42278d5a974c752b4600e77e4b47
DIPTARK BOSE <diptark.bose@gmail.com>
2017-01-15 18:22:39 +0530

    First Commit

---
+++ selection_sort/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter 5 numbers\n";
+    int i, arr[5], j, temp, minimum, pos;
+    for(i=0; i<5; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for(i=0; i<5; i++)
+    {
+        minimum=arr[i];
+        pos=0;
+        //Finding minimum in remaining list
+        for(j=i+1; j<5; j++)
+        {
+            if(minimum>arr[j])
+            {
+                minimum=arr[j];
+                pos=j;
+            }
+        }
+        //Swapping with minimum value
+        temp=arr[pos];
+        arr[pos]=arr[i];
+        arr[i]=temp;
+    }
+    cout<<"The sorted list is:\n";
+    for(i=0; i<5; i++)
+    {
+        cout<<arr[i]<<"\t";
+    }
+}
